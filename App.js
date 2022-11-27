a7c866aed484963e788bfeb567bc8654698a526f
wduf <wduf02@gmail.com>
2022-11-27 17:12:56 -0500

    updated one-page layout + removed router

--- App.js
+++ App.js
@@ -3,13 +3,39 @@ import { layout } from "./layout/Layout.js";
 import { Model } from "./model/Model.js";
 import { Boundary } from "./boundary/Boundary.js";
 import { Controller } from "./controller/Controller.js";
-
 function App()
 {
 	return (
 		<main className="App" style={layout.App}>
-			<img src="https://media.tenor.com/pY8hg1BiNMcAAAAC/the-bron-jame-basketball.gif" 
-			alt="the bron jame"/>
+			<header className="header" style={layout.header}>
+				<p className="punchender-logo" style={layout.punchender_logo}>Punchender</p>
+
+				{/* comment out either Register or the div to see what it will look like */}
+				
+				<p>Register</p>
+				<div className="header-user-wrapper" style={layout.header_user_wrapper}>
+					<p>JS: USERNAME</p>
+					<p className="header-vertical-divider" style={layout.header_vertical_divider}>|</p>
+					<p>Log Out</p>
+				</div>
+			</header>
+			<div className="header-line" style={layout.header_line}></div>
+			<div className="search-projects-wrapper" style={layout.search_projects_wrapper}>
+				<input className="search-projects-bar" type="text" placeholder="Search Projects..." style={layout.search_projects_bar}></input>
+				<div className="projects-list-wrapper" style={layout.projects_list_wrapper}>infinite scroll projects go here</div>
+			</div>
+
+			{/* hide all but current role */}
+
+			<div className="admin-wrapper" style={layout.admin_wrapper}>
+				<button className="reap-projects-button" style={layout.reap_projects_button}>Reap Projects</button>
+			</div>
+			<div className="developer-wrapper" style={layout.developer_wrapper}>
+				DEVELOPER ONLY STUFF GOES HERE
+			</div>
+			<div className="supporter-wrapper" style={layout.supporter_wrapper}>
+				SUPPORTER ONLY STUFF GOES HERE
+			</div>
 		</main>
 	);
 }
--- layout/Layout.js
+++ layout/Layout.js
@@ -2,6 +2,103 @@ export const layout =
 {
 	App:
 	{
-		background: "#e0bfff",
+		margin: 0,
+		padding: 0,
+		boxSizing: "border-box",
+		width: "100vw",
+		height: "100vh",
+		background: "#191919"
 	},
+
+	header:
+	{
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		padding: "0 2%",
+		fontSize: 20,
+		color: "white"
+		// border: "3px solid blue"
+	},
+
+	punchender_logo:
+	{
+		fontSize: 32,
+		fontFamily: "Courier New, Monospace",
+		color: "red"
+	},
+
+	header_user_wrapper:
+	{
+		display: "flex",
+		alignItems: "center",
+		gap: 20,
+		fontSize: 20,
+		color: "white"
+	},
+
+	header_vertical_divider:
+	{
+		fontSize: 24,
+		color: "red"
+	},
+
+	header_line:
+	{
+		background: "red",
+		height: 2,
+		margin: "0 1%"
+	},
+
+	search_projects_wrapper:
+	{
+		margin: "2%",
+		padding: "1% 4%",
+		border: "2px solid lime",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+
+	search_projects_bar:
+	{
+		background: "lightgray",
+		fontSize: 36,
+		height: 60,
+		width: "calc(100% - 20px)",
+		padding: "0 10px",
+		margin: "0 0 1%"
+	},
+
+	projects_list_wrapper:
+	{
+		border: "1px dotted yellow",
+		width: "100%",
+		height: 420,
+		margin: 10
+	},
+
+	admin_wrapper:
+	{
+		width: "fit-content",
+		margin: "auto"
+	},
+
+	reap_projects_button:
+	{
+		fontSize: 40,
+		color: "red"
+	},
+
+	developer_wrapper:
+	{
+		border: "1px double lightblue",
+		margin: "2%"
+	},
+
+	supporter_wrapper:
+	{
+		border: "1px double lightblue",
+		margin: "2%"
+	}
 };
