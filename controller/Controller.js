d81ec76725d6ee07a45de0adf7da8cebdf25cd29
wduf <wduf02@gmail.com>
2022-12-01 15:39:20 -0500

    create project wip, mysql error

--- controller/Controller.js
+++ controller/Controller.js
@@ -3,6 +3,8 @@ import { Supporter } from "../model/Supporter";
 import { Admin } from "../model/Admin";
 import { Project } from "../model/Project";
 
+var base_url = "https://g6kz80jtk5.execute-api.us-east-1.amazonaws.com/Prod/";
+
 export class Controller
 {
 	static logIn(model, user, pw)
@@ -38,18 +40,38 @@ export class Controller
 
 	static register(model, user, pw, role)
 	{
-		for(let i = 0; i < model.db.users.length; i++)
+		let data =
 		{
-			let cur = model.db.users[i];
-			if(cur.username === user)
+			"username": user,
+			"password": pw,
+			"role": role
+		};
+		let body = JSON.stringify(data);
+		let xhr = new XMLHttpRequest();
+		let add_url = (base_url + "register");
+		xhr.open("POST", add_url, true);
+		xhr.send(body);
+		xhr.onloadend = function () {
+		if (xhr.readyState === XMLHttpRequest.DONE)
+		{
+			// console.log(JSON.parse(xhr.response).statusCode)
+			let code = JSON.parse(xhr.response).statusCode
+			if(code === 200)
+			{
+				console.log(code)
+				alert("successfully registered.");
+			}
+			else
 			{
 				alert("cannot register. there is already a user with that name.");
-				return;
 			}
 		}
-		// got through without issues
-		model.db.users.push({"username": user, "password": pw, "role": role});
-		alert("successfully registered.");
+		else
+		{
+			alert("Error: " + xhr.response.statusCode);
+		}
+		console.log(xhr.response)
+		}
 	}
 
 	static fetchSupporterActivity(model)
@@ -129,34 +151,56 @@ export class Controller
 
 	static createProject(model, name, type, story, goal, deadline)
 	{
-		for(let i = 0; i < model.db.projects.length; i++)
+		let data =
 		{
-			let cur = model.db.projects[i];
-			if((cur.name === name) && (cur.creator === model.user.username))
+			"name": name,
+			"type": type,
+			"story": story,
+			"goal": goal,
+			"deadline": deadline
+		};
+		let body = JSON.stringify(data);
+		let xhr = new XMLHttpRequest();
+		let add_url = base_url + "project"
+		xhr.open("POST", add_url, true);
+		xhr.send(body);
+		xhr.onloadend = function () {
+		let info = JSON.parse(xhr.response)
+		if(xhr.readyState === XMLHttpRequest.DONE) 
+		{
+			console.log(JSON.parse(xhr.response).statusCode);
+			if(info.code === 200)
 			{
-				alert("cannot make two projects with the same name by the same person.");
-				return;
+				console.log(info.code);
+				alert("successfully registered.");
+			}
+			else
+			{
+				alert("error occurred. cannot create project.");
 			}
 		}
-		// got through, no issues
-		model.db.projects.push({"name": name, "creator": model.user.username, "type": type, "story": story, "amount": 0, "goal": goal, "deadline": deadline})
-		alert("project created");
+		else
+		{
+			alert("Error: " + info.statusCode);
+		}
+		console.log(xhr);
+		}
 	}
 
 	// TODO: add pledge, need to know project id
-	static createPledge(model, amount, reward, max)
-	{
-		for(let i = 0; i < model.db.pledges.length; i++)
-		{
-			let cur = model.db.projects[i];
-			if((cur.name === name) && (cur.creator === model.user.username))
-			{
-				alert("cannot make two projects with the same name by the same person.");
-				return;
-			}
-		}
-		// got through, no issues
-		model.db.projects.push({"name": name, "creator": model.user.username, "type": type, "story": story, "amount": 0, "goal": goal, "deadline": deadline})
-		alert("project created");
-	}
+	// static createPledge(model, amount, reward, max)
+	// {
+	// 	for(let i = 0; i < model.db.pledges.length; i++)
+	// 	{
+	// 		let cur = model.db.projects[i];
+	// 		if((cur.name === name) && (cur.creator === model.user.username))
+	// 		{
+	// 			alert("cannot make two projects with the same name by the same person.");
+	// 			return;
+	// 		}
+	// 	}
+	// 	// got through, no issues
+	// 	model.db.projects.push({"name": name, "creator": model.user.username, "type": type, "story": story, "amount": 0, "goal": goal, "deadline": deadline})
+	// 	alert("project created");
+	// }
 }
