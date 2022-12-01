c6ab98802366dbdadd3bde7d3af998c10e234666
venus <kiritout@gmail.com>
2022-12-01 12:58:14 -0500

    updating register designer

--- controller/Controller.js
+++ controller/Controller.js
@@ -3,6 +3,8 @@ import { Supporter } from "../model/Supporter";
 import { Admin } from "../model/Admin";
 import { Project } from "../model/Project";
 
+var base_url = "https://g6kz80jtk5.execute-api.us-east-1.amazonaws.com/Prod/";
+
 export class Controller
 {
 	static logIn(model, user, pw)
@@ -38,18 +40,67 @@ export class Controller
 
 	static register(model, user, pw, role)
 	{
-		for(let i = 0; i < model.db.users.length; i++)
-		{
-			let cur = model.db.users[i];
-			if(cur.username === user)
-			{
-				alert("cannot register. there is already a user with that name.");
-				return;
+		let data = {};
+		data["username"] = user;
+		data["password"] = pw;
+		
+		console.log(user)
+		console.log(pw)
+
+	
+		let add_url = base_url + "designer"
+		console.log(data)
+
+		let body = JSON.stringify(data);
+		
+		console.log(body)
+
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", add_url, true);
+		
+		// var body2 = {
+		// 	"body": body
+		// }
+
+		// var body3 = JSON.stringify(body2);
+
+		// var body2 = {
+		// 	"body": "{\n    \"username\": \"yoyo4\",\n    \"password\": \"yoyo4\"\n}"
+		//   };
+		// console.log(body3)
+		xhr.send(body);
+
+		xhr.onloadend = function () {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				// let response =  JSON.parse(xhr.body);
+				console.log(JSON.parse(xhr.response).statusCode)
+				let code = JSON.parse(xhr.response).statusCode
+				if(code === 200){
+					console.log(code)
+					alert("successfully registered.");
+				}
+				else {
+					alert("cannot register. there is already a user with that name.");
+				}
 			}
+			else {
+				alert("Error: " + xhr.response.statusCode);
+			}
+			console.log(xhr.response)
 		}
+
+		// for(let i = 0; i < model.db.users.length; i++)
+		// {
+		// 	let cur = model.db.users[i];
+		// 	if(cur.username === user)
+		// 	{
+		// 		alert("cannot register. there is already a user with that name.");
+		// 		return;
+		// 	}
+		// }
 		// got through without issues
-		model.db.users.push({"username": user, "password": pw, "role": role});
-		alert("successfully registered.");
+		// model.db.users.push({"username": user, "password": pw, "role": role});
+		// alert("successfully registered.");
 	}
 
 	static fetchSupporterActivity(model)
@@ -144,19 +195,19 @@ export class Controller
 	}
 
 	// TODO: add pledge, need to know project id
-	static createPledge(model, amount, reward, max)
-	{
-		for(let i = 0; i < model.db.pledges.length; i++)
-		{
-			let cur = model.db.projects[i];
-			if((cur.name === name) && (cur.creator === model.user.username))
-			{
-				alert("cannot make two projects with the same name by the same person.");
-				return;
-			}
-		}
-		// got through, no issues
-		model.db.projects.push({"name": name, "creator": model.user.username, "type": type, "story": story, "amount": 0, "goal": goal, "deadline": deadline})
-		alert("project created");
-	}
+	// static createPledge(model, amount, reward, max)
+	// {
+	// 	for(let i = 0; i < model.db.pledges.length; i++)
+	// 	{
+	// 		let cur = model.db.projects[i];
+	// 		if((cur.name === name) && (cur.creator === model.user.username))
+	// 		{
+	// 			alert("cannot make two projects with the same name by the same person.");
+	// 			return;
+	// 		}
+	// 	}
+	// 	// got through, no issues
+	// 	model.db.projects.push({"name": name, "creator": model.user.username, "type": type, "story": story, "amount": 0, "goal": goal, "deadline": deadline})
+	// 	alert("project created");
+	// }
 }
